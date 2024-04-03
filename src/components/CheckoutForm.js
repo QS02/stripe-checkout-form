bb091d48baa58c9d4cac393c48714244d33d76e5
Eric Wong <ericzwong08@gmail.com>
2024-04-03 09:44:10 -0400

    Show payment amount

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import {
   PaymentElement,
   useStripe,
@@ -6,10 +7,14 @@ import {
 } from '@stripe/react-stripe-js';
 
 export default function CheckoutForm({ clientSecret }) {
+  const router = useRouter();
   const stripe = useStripe();
   const elements = useElements();
 
   const [message, setMessage] = useState(null);
+  const [amount, setAmount] = useState(0);
+  const [hasError, setHasError] = useState(false);
+
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -18,23 +23,23 @@ export default function CheckoutForm({ clientSecret }) {
     }
 
     stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
-      if (!paymentIntent) {
-        setMessage('Invalid payment intent.');
-        return;
-      }
-      
-      switch (paymentIntent.status) {
+      console.log('paymentIntent', paymentIntent);
+
+      switch (paymentIntent?.status) {
         case 'succeeded':
-          setMessage('Payment succeeded!');
+          setMessage('Payment already succeeded!');
           break;
         case 'processing':
           setMessage('Your payment is processing.');
           break;
         case 'requires_payment_method':
-          setMessage('Your payment was not successful, please try again.');
+          setAmount(paymentIntent.amount / 100);
+          setMessage('Please submit your payment.');
+          setHasError(false);
           break;
         default:
-          setMessage('Something went wrong.');
+          setMessage('Invalid payment intent.');
+          setHasError(true);
           break;
       }
     });
@@ -42,6 +47,11 @@ export default function CheckoutForm({ clientSecret }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (hasError) {
+      router.push('/');
+      return;
+    }
 
     if (!stripe || !elements) {
       // Stripe.js hasn't yet loaded.
@@ -51,14 +61,16 @@ export default function CheckoutForm({ clientSecret }) {
 
     setIsLoading(true);
 
-    const { error } = await stripe.confirmPayment({
+    const confirm = await stripe.confirmPayment({
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
         return_url: 'http://localhost:3000/success',
       },
     });
+    console.log('confirm', confirm);
 
+    const { error } = confirm;
     // This point will only be reached if there is an immediate error when
     // confirming the payment. Otherwise, your customer will be redirected to
     // your `return_url`. For some payment methods like iDEAL, your customer will
@@ -80,11 +92,35 @@ export default function CheckoutForm({ clientSecret }) {
   return (
     <form id="payment-form" onSubmit={handleSubmit}>
       <PaymentElement id="payment-element" options={paymentElementOptions} />
-      <button disabled={isLoading || !stripe || !elements} id="submit">
-        <span id="button-text">
-          {isLoading ? <div className="spinner" id="spinner"></div> : 'Pay now'}
-        </span>
-      </button>
+      <hr />
+      <div
+        style={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div>
+          <h5>Payment Amount: ${amount}</h5>
+        </div>
+        <button
+          style={{ maxWidth: '200px' }}
+          disabled={isLoading || !stripe || !elements}
+          id="submit"
+        >
+          <span id="button-text">
+            {isLoading ? (
+              <div className="spinner" id="spinner"></div>
+            ) : hasError ? (
+              'Return to Home'
+            ) : (
+              'Pay now'
+            )}
+          </span>
+        </button>
+      </div>
       {/* Show any error or success messages */}
       {message && <div id="payment-message">{message}</div>}
     </form>
